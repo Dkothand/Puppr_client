9e43364a9979808618a63536996077f3d452c810
Deva <dkothand@gmail.com>
2019-06-03 10:17:31 -0400

    set state in DogList component to dogs returned from index route, render names as list to DOM

--- src/components/DogList.js
+++ src/components/DogList.js
@@ -1,18 +1,40 @@
 import React from 'react'
 
 class DogList extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            dogs: []
+        }
+    }
     componentDidMount() {
+        // Gets dogs from index route on component mount
         this.getDogs()
     }
     getDogs() {
+        // Returns array of dogs from API /dogs index route and sets this.state.dogs to returned json
         fetch('/dogs')
         .then(res => res.json())
-        .then(resJSON => console.log(resJSON.dogs))
+        .then(resJSON => {
+            console.log(resJSON.dogs)
+            this.setState({
+                dogs: resJSON.dogs
+            })
+        })
         .catch(err => console.error(err))
     }
     render() {
         return(
-            <h2>List of Dogs</h2>
+            <>
+                <h2>List of Dogs</h2>
+                <ul>
+                    {this.state.dogs.map(dog => {
+                        return(
+                            <li key={dog.id}>{dog.name}</li>
+                        )
+                    })}
+                </ul>
+            </>
         )
     }
 }
