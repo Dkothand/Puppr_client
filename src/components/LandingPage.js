3e657be14b4a9656dcb5411e9ad52061673200f2
Deva <dkothand@gmail.com>
2019-06-06 18:40:23 -0400

    added video to landing page and started css

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,17 +1,27 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
 
+import '../css/LandingPage.css'
+import webm from '../assets/Border-Collies.webm'
+import mp4 from '../assets/Border-Collies.mp4'
+
 class LandingPage extends React.Component {
     render() {
         return (
             <div>
-                <h1>Puppr</h1>
-                <Link to="/login">
-                    <button>Register</button>
-                </Link>
-                <Link to="/home">
-                    <button>Browse</button>
-                </Link>
+                <video autoPlay={true} muted={true} loop id="bgvid">
+                    <source src={webm} type="video/webm"/>
+                    <source src={mp4} type={"video/mp4"}/>
+                </video>
+                <header className="landing-content">
+                    <h1>Puppr</h1>
+                    <Link to="/login">
+                        <button>Register</button>
+                    </Link>
+                    <Link to="/home">
+                        <button>Browse</button>
+                    </Link>
+                </header>
             </div>
         )
     }
